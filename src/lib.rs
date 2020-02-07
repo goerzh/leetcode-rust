694be6ed62c18ba35517e195de69175c4760dc49
goerzh <zhpan188@gmail.com>
2020-02-08 00:02:57 +0800

    s0001_two_sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
-pub mod s20_valid_parentheses;
+pub mod s0020_valid_parentheses;
+pub mod s0001_two_sum;
---
+++ src/s0001_two_sum.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut result: Vec<i32> = Vec::new();
+        let mut pair: HashMap<i32, i32> = HashMap::new();
+
+        for (i, elm) in nums.iter().enumerate() {
+            if pair.contains_key(elm) {
+                result.push(pair.get(elm).unwrap().clone());
+                result.push(i as i32);
+            } else {
+                pair.insert(target - elm.clone(), i as i32);
+            }
+        }
+
+        result
+    }
+}

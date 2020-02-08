81999981230400e62bdc1e7b882ba05e534cc0fe
goerzh <zhpan188@gmail.com>
2020-02-08 15:04:57 +0800

    less memory

--- src/s0015_3sum.rs
+++ src/s0015_3sum.rs
@@ -4,54 +4,65 @@ struct Solution;
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() < 3 {
+            return vec![];
+        }
+
         let mut result: Vec<Vec<i32>> = Vec::new();
-        for (i, elm) in nums.iter().enumerate() {
-            let mut temp = Solution::two_sum(nums[i+1..].iter().cloned().collect(), 0-elm.clone());
+        let mut nums = nums;
+        nums.sort();
+        let mut previous = nums[0] - 1;
+        let mut i = 0;
+        while i < nums.len() - 2 {
+            if nums[i] == previous {
+                i += 1;
+                continue;
+            }
+
+            previous = nums[i];
+
+            let mut temp = Solution::two_sum(&nums[i+1..], 0-nums[i]);
             for v in temp.iter_mut() {
-                v.push(elm.clone());
+                v.insert(0, nums[i]);
             }
             result.extend(temp);
+
+            i += 1;
         }
-        result = Solution::sort(result);
-        result = Solution::dedup(result);
 
         result
     }
 
-    pub fn dedup(mut input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let mut temp: HashSet<Vec<i32>> = HashSet::new();
-        let mut result: Vec<Vec<i32>> = Vec::new();
-        for elm in input.into_iter() {
-            temp.insert(elm);
-        }
-
-        for elm in temp.into_iter() {
-            result.push(elm);
-        }
-        result
-    }
-
-    pub fn sort(mut input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        for v in input.iter_mut() {
-            v.sort()
-        }
-        input
-    }
-
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    pub fn two_sum(nums: &[i32], target: i32) -> Vec<Vec<i32>> {
         let mut pair: HashMap<i32, i32> = HashMap::new();
         let mut result: Vec<Vec<i32>> = Vec::new();
-        for elm in nums.iter() {
-            if pair.contains_key(elm) {
+        let mut i = 0;
+        while i < nums.len() {
+            if pair.contains_key(&nums[i]) {
                 let mut temp: Vec<i32> = Vec::new();
-                temp.push(elm.clone());
-                temp.push(pair.get(elm).unwrap().clone());
+                temp.push(pair.get(&nums[i]).unwrap().clone());
+                temp.push(nums[i]);
+
                 result.push(temp);
+
+                i = Solution::next_unique(nums, i, true);
             } else {
-                pair.insert(target - elm.clone(), elm.clone());
+                pair.insert(target - nums[i], nums[i].clone());
+
+                i += 1;
             }
         }
+
         result
+    }
+
+    pub fn next_unique(nums: &[i32], idx: usize, forward: bool) -> usize {
+        let curr = nums[idx];
+        let mut i = idx;
+        while i > 0 && i < nums.len() && nums[i] == curr {
+            i = if forward {i + 1} else { i - 1 }
+        }
+        i
     }
 }
 
@@ -60,8 +71,11 @@ mod tests {
     #[test]
     pub fn test_solution() {
         use super::*;
-        assert!(vec![1,2,3]==vec![1,2,3]);
-        assert_eq!(Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
-                   vec![vec![-1, 0, 1], vec![-1, -1, 2]]);
+//        assert_eq!(Solution::three_sum(vec![0,-4,-1,-4,-2,-3,2]),
+//                   vec![vec![-2, 0, 2]]);
+//        assert_eq!(Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
+//                   vec![vec![-1, 0, 1], vec![-1, -1, 2]]);
+        assert_eq!(Solution::three_sum(vec![0,0,0,0]),
+                   vec![vec![0,0,0]]);
     }
 }
